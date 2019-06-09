e9d84f2ad736b0fbb5ec4b30dc923bd10ef2813e
Nickolas Gumeniuk <nickolasgumeniuk@gmail.com>
2019-06-09 20:51:54 +0300

    Implemented auth view in MainActivity

--- app/src/main/java/com/example/evoarchexample/MainActivity.kt
+++ app/src/main/java/com/example/evoarchexample/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.evoarchexample
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AuthView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun showSuccess() {
+        Toast.makeText(this, getString(R.string.success), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showAuthError() {
+        Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showEmailValidationError() {
+        email.error = getString(R.string.error)
+    }
+
+    override fun showPasswordValidationError() {
+        password.error = getString(R.string.error)
     }
 }
