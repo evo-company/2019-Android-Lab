571ae7d86d203ed0070a9dea5b8387c4e68df320
n.humeniuk <Rd5676dD>
2019-06-10 17:55:21 +0300

    Refactor

--- app/src/main/java/com/example/evoarchexample/data/AuthDataRepository.kt
+++ app/src/main/java/com/example/evoarchexample/data/AuthDataRepository.kt
@@ -1,5 +1,8 @@
 package com.example.evoarchexample.data
 
+import com.example.evoarchexample.errors.WrongPasswordException
+
 interface AuthDataRepository {
+    @Throws(WrongPasswordException::class)
     fun logIn(email: String, password: String): String
 }
--- app/src/main/java/com/example/evoarchexample/data/MockAuthDataRepository.kt
+++ app/src/main/java/com/example/evoarchexample/data/MockAuthDataRepository.kt
@@ -3,10 +3,18 @@ package com.example.evoarchexample.data
 import com.example.evoarchexample.errors.WrongPasswordException
 
 class MockAuthDataRepository : AuthDataRepository {
+
+    companion object {
+        private const val CORRECT_PASSWORD = "111111"
+        private const val CORRECT_RESPONSE = "a0dwKMD94"
+    }
+
+    @Throws(WrongPasswordException::class)
     override fun logIn(email: String, password: String): String {
-        if (password == "111111") {
-            return "token"
+        if (password == CORRECT_PASSWORD) {
+            return CORRECT_RESPONSE
+        } else {
+            throw WrongPasswordException()
         }
-        throw WrongPasswordException()
     }
 }
