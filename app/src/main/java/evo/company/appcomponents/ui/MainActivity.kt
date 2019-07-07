d7f8d34847b48cb51db70827c9f5df938f8fab38
androks <androks12@gmail.com>
2019-07-07 22:05:54 +0300

    Initialized base navigation graph

--- app/src/main/java/evo/company/appcomponents/ui/MainActivity.kt
+++ app/src/main/java/evo/company/appcomponents/ui/MainActivity.kt
@@ -16,30 +16,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //setupBottomNavigationBar()
+        setupBottomNavigationBar()
     }
 
-//    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-//        super.onRestoreInstanceState(savedInstanceState)
-//        // Now that BottomNavigationBar has restored its instance state
-//        // and its selectedItemId, we can proceed with setting up the
-//        // BottomNavigationBar with Navigation
-//        setupBottomNavigationBar()
-//    }
-//
-//    private fun setupBottomNavigationBar() {
-//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-//
-//        val navGraphIds = listOf(R.navigation.feed, R.navigation.developers)
-//
-//        // Setup the bottom navigation view with a list of navigation graphs
-//        val controller = bottomNavigationView.setupWithNavController(
-//            navGraphIds = navGraphIds,
-//            fragmentManager = supportFragmentManager,
-//            containerId = R.id.nav_host_container,
-//            intent = intent
-//        )
-//
-//        currentNavController = controller
-//    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        val navGraphIds = listOf(R.navigation.feed, R.navigation.developers)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
+
+        currentNavController = controller
+    }
 }
